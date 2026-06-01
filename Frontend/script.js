1dba94da0cce3ec3b725c6cc27f579d1724ef6be
Emmanuelle <you@example.com>
2026-06-01 10:54:21 -0300

    fim do java

---
+++ Frontend/script.js
@@ -0,0 +1,100 @@
+async function analyzenews() {
+
+    const text = document.getElementById("news").value;
+
+    if(text.trim()=== "") {
+        alert("Digite uma notícia.");
+        return;
+    }
+const resultDiv = document.getElementById("result");
+
+resultDiv.innerHTML = `
+    <div class="loading">
+         🧠 AI analisando notícia...
+     </div>
+`;
+
+try {
+
+    const response = await fetch(
+        "https://127.0.0.1:5000/predict",
+        {
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                text: text
+            })
+         }
+     );
+
+     const data = await response.json();
+
+     if(data.error) {
+
+        resultDiv.innerHTML = `
+            <div class="result-title fake">
+                ❌ Erro
+            </div>
+
+                <p>${data.error}</p>
+    `;
+
+    return;
+}
+
+   const resultClass =
+       data.result.includes("Fake")
+       ? "fake"
+       : "real";
+
+   resultDiv.innerHTML = `
+       <div class="result-title ${resultClass}">
+           ${data.result}
+        </div>
+
+        <div class="stats">
+        
+            <div class="stat-card">
+                <div class="stat-label">
+                    🔴 Fake News
+                </div>
+                
+                <div class=stat-value">
+                    ${data.fake_probability}% 
+                </div> 
+            </div>
+            <div class="stat-card">
+                <div class="stat-label">
+                    🟢 Noticia Real 
+                </div> 
+                
+                <div class="stat-value">
+                    ${data.true_probability}% 
+                </div>
+            </div>
+            
+        </div>
+
+        <div class="explanation">
+            📖 ${data.explanation}>
+        </div>
+    `;
+    } catch(error){
+     
+        console.error(error);
+        
+        resultDiv.innerHTML = `
+            <div class="result-title fake">
+                ❌ Erro ao conectar 
+            </div>
+            
+            <p> 
+                Não foi possível se conectar com a API. 
+                </p>
+        `;
+    }
+}
